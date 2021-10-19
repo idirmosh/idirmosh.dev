f84c7d0e86acd1c644c2d4ef97cdec36b57dccb5
flowidir <meedinbox@gmail.com>
2021-10-19 14:50:09 -0400

    🔧helper functions randGen/keyGen

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -6,3 +6,9 @@ export const reduceTags = (acc, { tags }) => acc.concat(tags);
 
 // returns frontMatter from post
 export const postMetaFilter = ({ body, _raw, _id, ...rest }) => rest;
+
+// Genarate random number
+export const randGen = (max) => Math.floor(Math.random() * max);
+
+// return unique prefix for react keys
+export const keyGen = (str) => `key-${[...str][randGen(str.length)]}-${str}`;
