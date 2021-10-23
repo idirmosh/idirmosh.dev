7c836f0c4c4cf50ae13e69712f9cda2184d89d99
flowidir <meedinbox@gmail.com>
2021-10-23 15:33:45 -0400

    👌 making the blog home page better by calling the api once to get all the views

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,19 +1,36 @@
 import type { GetStaticProps, NextPage } from 'next';
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { allPosts } from '.contentlayer/data';
 import PostCard from '@components/Post/Card';
-import { postMetaFilter } from '@lib/helpers';
-import { IPageProps } from 'global';
+import { postMetaFilter, subscribe } from '@lib/helpers';
+import { IPageProps, IView } from 'global';
+import { box } from '@styles/common';
 
 const Blog: NextPage<IPageProps> = ({ posts }) => {
+  const [slugs, setSlugs] = useState(null);
+  //console.log(slugs);
+  useEffect(() => {
+    let isSubscribed = true;
+    if (isSubscribed) subscribe(setSlugs, 'http://localhost:3000/api/views');
+    return () => {
+      isSubscribed = false;
+    };
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Blog</title>
       </Head>
       <h1>hi blog</h1>
-      {posts && posts.map((post) => <PostCard key={post.slug} post={post} />)}
+      <div className={box({ css: {} })}>
+        {slugs &&
+          posts &&
+          posts.map((post) => (
+            <PostCard viewProp={slugs && slugs[post.slug]} key={post.slug} post={post} />
+          ))}
+      </div>
 
       {/* 
       <h1>{frontmatter.title}</h1>
@@ -24,6 +41,7 @@ const Blog: NextPage<IPageProps> = ({ posts }) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = allPosts.map(postMetaFilter);
+
   return { props: { posts } };
 };
 
