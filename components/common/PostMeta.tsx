bea8df582056b142f1f5ff170cdd90c8951004e2
idir <meedinbox@gmail.com>
2021-12-03 13:23:47 -0500

    ✔ types

--- components/common/PostMeta.tsx
+++ components/common/PostMeta.tsx
@@ -1,14 +1,18 @@
 import { text } from '@styles/typography';
 import { format, parseISO } from 'date-fns';
-import React from 'react';
+import React, { FC } from 'react';
 
-function PostMeta({ date, readTime, ...rest }) {
+const PostMeta: FC<{ date: string; readTime: string; [x: string]: any }> = ({
+  date,
+  readTime,
+  ...rest
+}) => {
   return (
     <div {...rest}>
       <time className={text({ type: 'small' })}>{format(parseISO(date), 'MMM dd, yyyy')}</time>
       <span className={text({ type: 'small' })}>{readTime}</span>
     </div>
   );
-}
+};
 
 export default PostMeta;
