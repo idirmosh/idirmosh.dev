481ff731b6ddcbf7488410ef9690e6d6efb44812
idir <meedinbox@gmail.com>
2021-12-03 13:00:25 -0500

    style edit and types

--- components/common/BlogListWrapper.tsx
+++ components/common/BlogListWrapper.tsx
@@ -1,23 +1,15 @@
 import { BLOG_CARD_GAP, BLOG_CARD_WIDTH } from '@lib/constants';
-import { box, wrapper } from '@styles/common';
-import React from 'react';
+import { wrapper } from '@styles/common';
+import { FC, ReactNode } from 'react';
+import { css } from 'stitches.config';
 
-function BlogListWrapper(props) {
-  return (
-    <div
-      className={box(
-        wrapper({
-          css: {
-            display: 'grid',
-            gridTemplateColumns: `repeat(auto-fit, minmax(${BLOG_CARD_WIDTH}px, 1fr))`,
-            gridGap: `${BLOG_CARD_GAP}px`,
-          },
-        })
-      )}
-    >
-      {props.children}
-    </div>
-  );
-}
+const BlogListWrapper: FC<ReactNode> = ({ children }) => {
+  const grid = css({
+    display: 'grid',
+    gridTemplateColumns: `repeat(auto-fit, minmax(${BLOG_CARD_WIDTH}px, 1fr))`,
+    gridGap: `${BLOG_CARD_GAP}px`,
+  });
+  return <div className={grid(wrapper())}>{children}</div>;
+};
 
 export default BlogListWrapper;
