b8d753fb3f698d43c1dd7c3eaa5a4cc351fae752
idir <meedinbox@gmail.com>
2021-12-14 15:38:35 -0500

    tags page ui

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -6,67 +6,82 @@ import { filterTags, keyGen, randGen, reduceTags } from '@lib/helpers';
 import { ITag, ITags } from 'global';
 import { FC } from 'react';
 import { css } from 'stitches.config';
-import Link from 'next/link';
-
-const cont = css({
-  display: 'grid',
-  gridTemplateColumns: '1fr 1fr 1fr',
-  flexFlow: 'row wrap',
-  gridColumnGap: '18px',
-  gridRowGap: '18px',
-});
+import Layout from '@components/Layout';
+import { box, singlePostWrapper } from '@styles/common';
+import Link from '@components/common/Link';
+import { heading, text } from '@styles/typography';
 
 const TagsPage: NextPage<ITags> = ({ tags }) => {
+  const container = css({
+    display: 'grid',
+    gridAutoColumns: 'auto',
+    gridTemplateColumns: '1fr',
+    gridRowGap: '1rem',
+    gridColumnGap: '1rem',
+    '@mobile': {
+      gridTemplateColumns: '1fr 1fr',
+    },
+  });
+  const info = css({ margin: '2rem AUTO' });
   return (
-    <div className={styles.container({})}>
-      <div className={styles.box({ my: '$5' })}>
-        <h1>Tags</h1>
-        <div className={cont()}>
+    <Layout>
+      <div className={singlePostWrapper()}>
+        <div className={info()}>
+          <h1>TAGS</h1>
+        </div>
+        <div className={container()}>
           {tags.map((tag) => (
             <TagCard key={keyGen(tag)} tag={tag} />
           ))}
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
-const styules = css({
-  padding: '18px',
-  borderRadius: '6px',
-  transition: 'all .2s',
-  overflow: 'hidden',
-  backgroundColor: '$neutral6',
-  '&:hover': {
-    backgroundColor: '$neutral7',
-    boxShadow: '0 1px 2px -1px rgb(0 0 0 / 20%), 0 1px 9px 0 rgb(0 0 0 / 14%)',
-  },
-  a: {
-    textDecoration: 'none',
-    color: '$neutral0',
-  },
-  h3: {
-    fontSize: '19px',
-    textTransform: 'capitalize',
-    lineHeight: '24px',
-    marginTop: '9px',
-  },
-  p: {
-    fontSize: '14px',
-    lineHeight: '20px',
-    marginTop: '6px',
-    color: '$neutral1',
-  },
-});
-
 const TagCard = ({ tag }) => {
+  const card = css({
+    padding: '1rem 0',
+    '&:hover': {
+      boxShadow: '0 2px 2px -2px $colors$neutral4',
+    },
+  });
+  const container = css({
+    display: 'flex',
+    alignItems: 'center',
+  });
+  const circle = css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    minWidth: '2.4rem',
+    height: '2.4rem',
+    backgroundColor: '$neutral6',
+    borderRadius: '999px',
+    fontSize: '1.2rem',
+    fontWeight: '600',
+    textTransform: 'uppercase',
+    color: '$neutral4',
+  });
   return (
-    <article className={styules()}>
-      <Link href="/tags/[tag]" as={`/tags/${tag}`}>
-        <a>
-          <h3>{tag}</h3>
-          <p>{`Latest updates, tutorials, and news about ${tag}.`}</p>
-        </a>
+    <article className={card()}>
+      <Link
+        href={`/tags/${tag}`}
+        className={box({ css: { textDecoration: 'none', color: '$neutral0' } })}
+      >
+        <div className={container()}>
+          <div className={circle()}>#</div>
+          <span className={box({ css: { marginLeft: '1rem' } })}>
+            <h4
+              className={heading({ type: 'h4', css: { textTransform: 'capitalize', margin: '0' } })}
+            >
+              {tag}
+            </h4>
+            <p
+              className={text({ type: 'small', css: { margin: '2px 0 0 0' } })}
+            >{`Latest updates, tutorials, and news about ${tag}.`}</p>
+          </span>
+        </div>
       </Link>
     </article>
   );
@@ -74,6 +89,7 @@ const TagCard = ({ tag }) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const tags = allPosts.reduce(reduceTags, []).filter(filterTags);
+
   return { props: { tags } };
 };
 
