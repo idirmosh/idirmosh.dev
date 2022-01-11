9ea5573cbbb2eb8d5af06d53df8a197042cb3f7a
idirmosh <idir.mosh@gmail.com>
2022-01-11 12:38:58 -0500

    analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NextDocument, { Html, Main, NextScript, Head } from 'next/document';
 import { getCssText } from 'stitches.config';
-import { GA_TRACKING_ID } from '@lib/analytics';
+import { GA_ID } from '@lib/analytics';
 
 const INTER_FONT = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"';
 
@@ -16,17 +16,17 @@ export default class Document extends NextDocument {
           <link href={INTER_FONT} rel="preload" as="style" />
           <link href={INTER_FONT} rel="stylesheet" media="all" />
 
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
           <script
-            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
+              gtag('config', '${GA_ID}', {
                 page_path: window.location.pathname,
-            });`,
+            });
+          `,
             }}
           />
 
