6dd906192cdee0d0b6a6bdcf514a69dd363ca927
idirmosh <idir.mosh@gmail.com>
2022-01-27 13:15:04 -0500

    refactor

--- components/FeaturedWorks.tsx
+++ components/FeaturedWorks.tsx
@@ -6,25 +6,6 @@ import { heading } from '@styles/typography';
 import { css } from 'stitches.config';
 
 function FeaturedWorks({ projects }: { projects: IProject[] }): React.ReactElement {
-  const container = css({
-    display: 'grid',
-    gridTemplateColumns: `repeat(1,minmax(0,1fr))`,
-    gridGap: '4rem',
-    marginTop: '$4 !important',
-    '@mobile': {
-      gridTemplateColumns: `repeat(2,minmax(0,1fr))`,
-      gridGap: '2rem',
-    },
-    '@tablet': {
-      gridTemplateColumns: `repeat(3,minmax(0,1fr))`,
-      gridGap: '2.5rem',
-    },
-    '@desktop': {
-      gridTemplateColumns: `repeat(4,minmax(0,1fr))`,
-      gridGap: '3rem',
-    },
-  });
-
   return (
     <div className={wrapper({ css: { margin: '$6 auto' } })}>
       <h2 className={heading({ type: 'h2' })}>Selected Projects</h2>
@@ -35,4 +16,24 @@ function FeaturedWorks({ projects }: { projects: IProject[] }): React.ReactEleme
     </div>
   );
 }
+
+const container = css({
+  display: 'grid',
+  gridTemplateColumns: `repeat(1,minmax(0,1fr))`,
+  gridGap: '4rem',
+  marginTop: '$4 !important',
+  '@mobile': {
+    gridTemplateColumns: `repeat(2,minmax(0,1fr))`,
+    gridGap: '2rem',
+  },
+  '@tablet': {
+    gridTemplateColumns: `repeat(3,minmax(0,1fr))`,
+    gridGap: '2.5rem',
+  },
+  '@desktop': {
+    gridTemplateColumns: `repeat(4,minmax(0,1fr))`,
+    gridGap: '3rem',
+  },
+});
+
 export default FeaturedWorks;
